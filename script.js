54fd829f06c0fc8e5602092d84b3283c3c4b3218
Mauricio123-cloud <yeisontorred2@gmail.com>
2024-12-03 19:15:09 -0500

    Aprendiendo a hacer un menu despelgable

---
+++ script.js
@@ -0,0 +1,12 @@
+let nuemro1 = 8
+let numero2 = 2
+
+var suma = nuemro1 + numero2
+var resta = nuemro1 - numero2
+var produto = nuemro1 * numero2
+var division = nuemro1 / numero2
+
+console.log("la suma es:" + suma);
+console.log("la resta es:" + resta);
+console.log("el producto es:" + producto);
+console.log("la division es:" + division);
